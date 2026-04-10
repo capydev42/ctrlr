8a6f7d14e7c33a14ab90e27b64fa56ec745b5523
capydev42 <ger4ik777@gmail.com>
2026-04-10 14:13:10 +0200

    fix(shell): enable use-dev-tty feature for macOS zsh widget compability
    - enable the use-dev-tty feature in crossterm to fall back to /dev/tty
     when stdin/stdout is not a terminal. This hopefully fixes "Failed to initialize
    input reader" error on macOS.

--- src/main.rs
+++ src/main.rs
@@ -25,8 +25,8 @@ fn main() -> color_eyre::Result<()> {
 }
 
 pub fn run_tui(output_file: Option<String>) -> color_eyre::Result<Option<String>> {
-    if !atty::is(atty::Stream::Stdout) {
-        eprintln!("Error: ctrlr must be run from a terminal. Not a TTY.");
+    if !atty::is(atty::Stream::Stdin) || !atty::is(atty::Stream::Stdout) {
+        eprintln!("Error: ctrlr must be run from a terminal. Stdin or stdout is not a TTY.");
         return Ok(None);
     }
     let mut terminal = ratatui::init();
