5e44bea13147eb36e98156dbe73d1127de074955
capydev42 <ger4ik777@gmail.com>
2026-05-04 09:26:49 +0200

    fix(clippy): fix clippy warnings

--- src/input/import_export.rs
+++ src/input/import_export.rs
@@ -9,21 +9,19 @@ pub fn handle(state: &mut AppState, key: KeyEvent) -> Action {
         (KeyCode::Backspace, _) => {
             state.import_export_file_path.pop();
         }
-        (KeyCode::Up, _) | (KeyCode::Char('p'), KeyModifiers::CONTROL) => {
+        (KeyCode::Up, _) | (KeyCode::Char('p'), KeyModifiers::CONTROL)
             if state.import_export_mode == ImportExportMode::Import
-                || state.import_export_mode == ImportExportMode::ImportPreview
-            {
-                state.import_mode_index = 0;
-                state.import_preview = None;
-                state.import_export_mode = ImportExportMode::Import;
-            }
+                || state.import_export_mode == ImportExportMode::ImportPreview =>
+        {
+            state.import_mode_index = 0;
+            state.import_preview = None;
+            state.import_export_mode = ImportExportMode::Import;
         }
-        (KeyCode::Down, _) | (KeyCode::Char('n'), KeyModifiers::CONTROL) => {
+        (KeyCode::Down, _) | (KeyCode::Char('n'), KeyModifiers::CONTROL)
             if state.import_export_mode == ImportExportMode::Import
-                || state.import_export_mode == ImportExportMode::ImportPreview
-            {
-                state.import_mode_index = 1;
-            }
+                || state.import_export_mode == ImportExportMode::ImportPreview =>
+        {
+            state.import_mode_index = 1;
         }
         (KeyCode::Enter, _) => match state.import_export_mode {
             ImportExportMode::Export => {
